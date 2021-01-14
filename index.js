4604327746bbc025708d60f2ba0589d5008721b0
simonisfiction <simon.chesney@yahoo.com>
2021-01-14 12:30:38 -0800

    teat

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ for(var i = 0; i < keys.length; i++){
 // creates an event listener that will trigger a key animation and a
 // sound file(.ogg) with the correct note during a "click" event.
 
-
+//this is a comment
 
 // creates an event listener that will trigger a key animation and a
 // sound file(.ogg) with the correct note during a "keydown" event.
